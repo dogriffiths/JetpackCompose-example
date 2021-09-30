3a40aef385ca7e3463d656f2edae52cf52235d16
davidg <dogriffiths@gmail.com>
2021-09-30 14:26:57 +0100

    Changes: Thu 30 Sep 2021 14:26:57 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,7 +14,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Buy milk")
+            TextEditor()
         }
     }
+}
+
+@Composable
+fun TextEditor() {
+    Text("Buy milk")
 }
