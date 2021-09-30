48fc3dea697ee8d4ac9290a34e8c44744db6a4e5
davidg <dogriffiths@gmail.com>
2021-09-30 14:37:58 +0100

    Changes: Thu 30 Sep 2021 14:37:58 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -27,6 +29,7 @@ fun TextEditor() {
         TextField(
             value = "Buy milk",
             onValueChange = {},
+            modifier = Modifier.fillMaxWidth(),
         )
         Text("Complete!")
     }
