75e17f5c0d82f917212ab5895fb535b6cf4eeb43
davidg <dogriffiths@gmail.com>
2021-09-30 14:52:58 +0100

    Changes: Thu 30 Sep 2021 14:52:58 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,15 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +24,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TextEditor() {
-    var name by remember { mutableStateOf("Buy milk")}
+    var name by remember { mutableStateOf("Buy milk") }
+    var complete by remember { mutableStateOf(true) }
     Column {
         TextField(
             value = name,
@@ -38,8 +36,10 @@ fun TextEditor() {
         )
         Row {
             Checkbox(
-                checked = true,
-                onCheckedChange = {},
+                checked = complete,
+                onCheckedChange = {
+                    complete = it
+                },
             )
             Text("Complete?")
         }
