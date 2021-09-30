ae55568933f4782234559d8ae2cb76c2991ee14e
davidg <dogriffiths@gmail.com>
2021-09-30 15:58:54 +0100

    Changes: Thu 30 Sep 2021 15:58:54 BST

--- app/src/main/java/com/example/myapplication/TaskList.kt
+++ app/src/main/java/com/example/myapplication/TaskList.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,7 +11,10 @@ fun TaskList(tasks: List<Task>) {
     LazyColumn {
         items(tasks.size) {
             val task = tasks[it]
-            Text(task.name)
+            Row {
+                Text(task.name)
+                Text(if (task.complete) "✓" else "")
+            }
         }
     }
 }
