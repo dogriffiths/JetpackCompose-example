2e6bb32cc8fa0f3e1b9efc1392e5775c4a7544df
davidg <dogriffiths@gmail.com>
2021-10-09 14:45:32 +0100

    Changes: Sat  9 Oct 2021 14:45:32 BST

--- app/src/main/java/com/example/myapplication/TaskList.kt
+++ app/src/main/java/com/example/myapplication/TaskList.kt
@@ -3,10 +3,12 @@ package com.example.myapplication
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TaskList(tasks: List<Task>) {
@@ -20,7 +22,7 @@ fun TaskList(tasks: List<Task>) {
 @Composable
 fun TaskItem(task: Task) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Text(task.name)
