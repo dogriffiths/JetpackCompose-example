acbdb198a085fd2f352f039b7c724d384f27febf
davidg <dogriffiths@gmail.com>
2021-10-09 13:21:46 +0100

    Changes: Sat  9 Oct 2021 13:21:46 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -26,6 +24,6 @@ class MainActivity : ComponentActivity() {
 fun TaskEditor() {
     Column {
         TextField(value = "Buy milk!!!!!", onValueChange={})
-        Text("Complete!")
+        Checkbox(checked = true, onCheckedChange = {})
     }
 }
