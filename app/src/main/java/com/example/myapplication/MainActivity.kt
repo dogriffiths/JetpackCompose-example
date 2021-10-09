7f027622b402802949c038d0eff39030a88d31db
davidg <dogriffiths@gmail.com>
2021-10-09 13:25:25 +0100

    Changes: Sat  9 Oct 2021 13:25:25 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -24,7 +26,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TaskEditor() {
     Column {
-        TextField(value = "Buy milk!!!!!", onValueChange={})
+        TextField(
+            value = "Buy milk!!!!!",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth()
+        )
         Row {
             Checkbox(checked = true, onCheckedChange = {})
             Text("Complete?")
