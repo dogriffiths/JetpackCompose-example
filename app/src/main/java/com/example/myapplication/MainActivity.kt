a50a7e622c2bd52ecace0173af7b6b372336c3f6
davidg <dogriffiths@gmail.com>
2021-10-09 14:29:58 +0100

    Changes: Sat  9 Oct 2021 14:29:58 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.MutableLiveData
 
 class TaskDao {
-    private var _tasks: List<Task> = mutableListOf(Task(name = "Buy car", complete = true))
+    private var _tasks: MutableLiveData<List<Task>> = MutableLiveData(mutableListOf(Task(name = "Buy car", complete = true)))
 
     fun insert(task: Task) {
         val newTasks = mutableListOf<Task>()
         newTasks.add(task)
-        newTasks.addAll(_tasks)
-        _tasks = newTasks
+        _tasks.value?.let {
+            newTasks.addAll(it)
+        }
+        _tasks.postValue(newTasks)
     }
 
     fun getAll() = _tasks
@@ -24,13 +28,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val taskDao = TaskDao()
         setContent {
-//            var tasks by remember { mutableStateOf(mutableListOf<Task>())}
-            val tasks = taskDao.getAll()
+            val tasks = taskDao.getAll().observeAsState()
             Column {
                 TaskEditor(Task(name = "", complete = false), onTaskChange = {
                     taskDao.insert(it)
                 })
-                TaskList(tasks)
+                tasks.value?.let {
+                    TaskList(it)
+                }
             }
         }
     }
