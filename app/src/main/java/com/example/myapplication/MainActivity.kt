1328c0d462e8e0f710e740f17e3b282769c67ea5
davidg <dogriffiths@gmail.com>
2021-10-09 14:19:01 +0100

    Changes: Sat  9 Oct 2021 14:19:01 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,17 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.*
 
+class TaskDao {
+    private var _tasks: List<Task> = mutableListOf(Task(name = "Buy car", complete = true))
+
+    fun insert(task: Task) {
+        val newTasks = mutableListOf<Task>()
+        newTasks.add(task)
+        newTasks.addAll(_tasks)
+        _tasks = newTasks
+    }
+
+    fun getAll() = _tasks
+}
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val taskDao = TaskDao()
         setContent {
-            var tasks by remember { mutableStateOf(mutableListOf<Task>())}
+//            var tasks by remember { mutableStateOf(mutableListOf<Task>())}
+            val tasks = taskDao.getAll()
             Column {
                 TaskEditor(Task(name = "", complete = false), onTaskChange = {
-                    val newTasks = mutableListOf<Task>()
-                    newTasks.add(it)
-                    newTasks.addAll(tasks)
-                    tasks = newTasks
+                    taskDao.insert(it)
                 })
                 TaskList(tasks)
             }
