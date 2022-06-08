2f9348e2546a6d4b355fdff45ef63097bccef52f
davidg <dogriffiths@gmail.com>
2022-06-08 13:22:22 +0100

    Changes: Wed  8 Jun 2022 13:22:22 BST

--- app/src/main/java/com/example/myapplication/viewmodels.kt
+++ app/src/main/java/com/example/myapplication/viewmodels.kt
@@ -17,6 +17,14 @@ class AddTaskScreenViewModel(val taskDao: TaskDao): ViewModel() {
     }
 }
 
+class EditTaskScreenViewModel(val taskDao: TaskDao): ViewModel() {
+    fun addTask(task: Task) {
+        viewModelScope.launch {
+            taskDao.insert(task)
+        }
+    }
+}
+
 class MyViewModelFactory(val taskDao: TaskDao): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TasksScreenViewModel::class.java)) {
