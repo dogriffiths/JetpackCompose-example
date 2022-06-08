83d8410f0d68cc9ea6cb789c5917b478771f6bd0
davidg <dogriffiths@gmail.com>
2022-06-08 12:31:50 +0100

    Changes: Wed  8 Jun 2022 12:31:50 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +11,6 @@ class MainActivity : ComponentActivity() {
         val vmf = MyViewModelFactory(taskDao)
         setContent {
             TasksScreen(vmf)
-        }
-    }
-}
-
-@Composable
-fun TasksScreen(vmf: MyViewModelFactory) {
-    val vm: TasksScreenViewModel = viewModel(factory = vmf)
-    val tasks by vm.getTasks().observeAsState()
-    Column {
-        TaskEditor(
-            task = Task(name = "", complete = false),
-            onTaskChange = {
-                vm.addTask(it)
-            }
-        )
-        tasks?.let {
-            TaskList(it)
         }
     }
 }
--- app/src/main/java/com/example/myapplication/TasksScreen.kt
+++ app/src/main/java/com/example/myapplication/TasksScreen.kt
@@ -1,2 +1,24 @@
-package com.example.myapplication 
+package com.example.myapplication
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun TasksScreen(vmf: MyViewModelFactory) {
+    val vm: TasksScreenViewModel = viewModel(factory = vmf)
+    val tasks by vm.getTasks().observeAsState()
+    Column {
+        TaskEditor(
+            task = Task(name = "", complete = false),
+            onTaskChange = {
+                vm.addTask(it)
+            }
+        )
+        tasks?.let {
+            TaskList(it)
+        }
+    }
+}
