55f875d788718891623577760a39d1d3eb7eefb7
davidg <dogriffiths@gmail.com>
2022-06-08 11:21:14 +0100

    Changes: Wed  8 Jun 2022 11:21:14 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,41 +32,6 @@ class MainActivity : ComponentActivity() {
 }
 
 data class Task(var name: String, var complete: Boolean)
-
-@Composable
-fun TaskEditor(task: Task, onTaskChange: (Task) -> Unit) {
-    var name by rememberSaveable(task.name) { mutableStateOf(task.name) }
-    var complete by rememberSaveable(task.complete) { mutableStateOf(task.complete) }
-    Column {
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = name,
-            onValueChange = {
-                name = it
-            },
-        )
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Checkbox(
-                checked = complete,
-                onCheckedChange = {
-                    complete = it
-                },
-            )
-            Text("Complete?")
-        }
-        Button(
-            onClick = {
-                onTaskChange(task.copy(name = name, complete = complete))
-                name = ""
-                complete = false
-            },
-        ) {
-            Text("SAVE")
-        }
-    }
-}
 
 //@Preview
 //@Composable
--- app/src/main/java/com/example/myapplication/TaskEditor.kt
+++ app/src/main/java/com/example/myapplication/TaskEditor.kt
@@ -1,2 +1,51 @@
-package com.example.myapplication 
+package com.example.myapplication
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TaskEditor(task: Task, onTaskChange: (Task) -> Unit) {
+    var name by rememberSaveable(task.name) { mutableStateOf(task.name) }
+    var complete by rememberSaveable(task.complete) { mutableStateOf(task.complete) }
+    Column {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = name,
+            onValueChange = {
+                name = it
+            },
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(
+                checked = complete,
+                onCheckedChange = {
+                    complete = it
+                },
+            )
+            Text("Complete?")
+        }
+        Button(
+            onClick = {
+                onTaskChange(task.copy(name = name, complete = complete))
+                name = ""
+                complete = false
+            },
+        ) {
+            Text("SAVE")
+        }
+    }
+}
