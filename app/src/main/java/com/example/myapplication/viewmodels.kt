d42de9ece43ac1688f2af60be3636ec3248609a8
davidg <dogriffiths@gmail.com>
2022-06-08 13:04:29 +0100

    Changes: Wed  8 Jun 2022 13:04:29 BST

--- app/src/main/java/com/example/myapplication/viewmodels.kt
+++ app/src/main/java/com/example/myapplication/viewmodels.kt
@@ -6,19 +6,24 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class TasksScreenViewModel(val taskDao: TaskDao): ViewModel() {
+    fun getTasks() = taskDao.getAll()
+}
+
+class AddTaskScreenViewModel(val taskDao: TaskDao): ViewModel() {
     fun addTask(task: Task) {
         viewModelScope.launch {
             taskDao.insert(task)
         }
     }
-
-    fun getTasks() = taskDao.getAll()
 }
 
 class MyViewModelFactory(val taskDao: TaskDao): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TasksScreenViewModel::class.java)) {
             return TasksScreenViewModel(taskDao) as T
+        }
+        if (modelClass.isAssignableFrom(AddTaskScreenViewModel::class.java)) {
+            return AddTaskScreenViewModel(taskDao) as T
         }
         throw IllegalArgumentException("Cannot find a viewmodel you dope...")
     }
