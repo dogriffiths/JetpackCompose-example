b872abe67baa097564eec38250544110fb0b9c51
davidg <dogriffiths@gmail.com>
2022-06-08 13:31:45 +0100

    Changes: Wed  8 Jun 2022 13:31:45 BST

--- app/src/main/java/com/example/myapplication/MainNavigation.kt
+++ app/src/main/java/com/example/myapplication/MainNavigation.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun MainNavigation(vmf: MyViewModelFactory) {
@@ -28,9 +30,17 @@ fun MainNavigation(vmf: MyViewModelFactory) {
         }
         composable(
             "tasks/{taskId}",
+            arguments = listOf(
+                navArgument("taskId") {
+                    type = NavType.LongType
+                }
+            )
         ) {
-            EditTaskScreen(vmf, taskId = 17L) {
-                navController.navigate("tasks")
+            it.arguments?.let {
+                val taskId = it.get("taskId") as Long
+                EditTaskScreen(vmf, taskId = 17L) {
+                    navController.navigate("tasks")
+                }
             }
         }
     }
