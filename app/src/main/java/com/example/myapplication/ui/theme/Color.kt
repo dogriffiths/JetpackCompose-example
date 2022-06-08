9784122b66cdeed9c7f62831b56b1b4599ba32a0
davidg <dogriffiths@gmail.com>
2022-06-08 13:54:53 +0100

    Changes: Wed  8 Jun 2022 13:54:53 BST

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200 = Color(0xFFFF9800)
+val Purple500 = Color(0xFFEE00FF)
+val Purple700 = Color(0xFFFF0067)
+val Teal200 = Color(0xFF008EFF)
