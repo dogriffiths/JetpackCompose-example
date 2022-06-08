72fe30cceb47e4c35f89c5aaaaac563424878279
davidg <dogriffiths@gmail.com>
2022-06-08 10:18:08 +0100

    Changes: Wed  8 Jun 2022 10:18:08 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -23,8 +24,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskEditor() {
-    Text("Buy milk!!!!")
-    Text("Complete!")
+    Column {
+        Text("Buy milk!!!!")
+        Text("Complete!")
+    }
 }
 
 //@Preview
