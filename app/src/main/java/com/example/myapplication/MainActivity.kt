295e8cf6e104cb1a4807eed5987425b4d78cba5a
davidg <dogriffiths@gmail.com>
2022-06-08 12:23:49 +0100

    Changes: Wed  8 Jun 2022 12:23:49 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class TasksScreenViewModel(val taskDao: TaskDao): ViewModel() {
     fun addTask(task: Task) {
@@ -16,7 +17,14 @@ class TasksScreenViewModel(val taskDao: TaskDao): ViewModel() {
     fun getTasks() = taskDao.getAll()
 }
 
-
+class MyViewModelFactory(val taskDao: TaskDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TasksScreenViewModel::class.java)) {
+            return TasksScreenViewModel(taskDao) as T
+        }
+        throw IllegalArgumentException("Cannot find a viewmodel you dope...")
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
