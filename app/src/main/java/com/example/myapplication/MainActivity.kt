5b30935d6db364d33c50265b31f1b4e40662213c
davidg <dogriffiths@gmail.com>
2022-06-08 11:50:21 +0100

    Changes: Wed  8 Jun 2022 11:50:21 BST

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,27 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.lifecycle.MutableLiveData
-
-data class Task(var name: String, var complete: Boolean)
-class TaskDao {
-    private var _tasks: MutableLiveData<List<Task>> = MutableLiveData(
-        mutableListOf(Task(name = "Buy milk", complete = true))
-    )
-
-    fun insert(task: Task) {
-        val newTasks = mutableListOf<Task>()
-        newTasks.add(task)
-        newTasks.addAll(_tasks.value!!)
-//        _tasks = newTasks
-        _tasks.postValue(newTasks)
-    }
-
-    fun getAll() = _tasks
-}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/myapplication/datastuff.kt
+++ app/src/main/java/com/example/myapplication/datastuff.kt
@@ -1,2 +1,20 @@
-package com.example.myapplication 
+package com.example.myapplication
 
+import androidx.lifecycle.MutableLiveData
+
+data class Task(var name: String, var complete: Boolean)
+class TaskDao {
+    private var _tasks: MutableLiveData<List<Task>> = MutableLiveData(
+        mutableListOf(Task(name = "Buy milk", complete = true))
+    )
+
+    fun insert(task: Task) {
+        val newTasks = mutableListOf<Task>()
+        newTasks.add(task)
+        newTasks.addAll(_tasks.value!!)
+//        _tasks = newTasks
+        _tasks.postValue(newTasks)
+    }
+
+    fun getAll() = _tasks
+}
