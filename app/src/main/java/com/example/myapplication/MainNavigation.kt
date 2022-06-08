c17cdef401d97ada6b0d791e510e46ef33fe4077
davidg <dogriffiths@gmail.com>
2022-06-08 13:20:59 +0100

    Changes: Wed  8 Jun 2022 13:20:59 BST

--- app/src/main/java/com/example/myapplication/MainNavigation.kt
+++ app/src/main/java/com/example/myapplication/MainNavigation.kt
@@ -1,2 +1,30 @@
-package com.example.myapplication 
+package com.example.myapplication
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainNavigation(vmf: MyViewModelFactory) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "tasks"
+    ) {
+        composable("tasks") {
+            TasksScreen(
+                vmf = vmf,
+                onAddTask = {
+                    navController.navigate("tasks/add")
+                }
+            )
+        }
+        composable("tasks/add") {
+            AddTaskScreen(vmf) {
+                navController.navigate("tasks")
+            }
+        }
+    }
+}
